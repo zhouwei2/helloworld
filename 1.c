716f3ef3bbb36134785ddf1048a29670514cb53a
zhouwei <zhouwei2@goodix.com>
2017-05-13 06:03:27 -0700

    all

--- 1.c
+++ 1.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 #include <stdio.h>
 
 int main(void)
@@ -7,7 +5,6 @@ int main(void)
 	printf("hello world\n");
 	return 0;
 }
-=======
+
 lsdkfjsldkjfsldkjf
 sdlkfsjdflk
->>>>>>> 38848ea0d2054c98fe16439225726467581ff647
